4c4955a1e0c4ed98b6f45e2f4c48b1e5cfe433e0
Philip Bui <philip.bui.developer@gmail.com>
2019-12-03 23:35:32 +1100

    Added class static property rules, component sort method rules, ESLint comment rules, Jest rules and comments

--- index.js
+++ index.js
@@ -2,45 +2,115 @@ module.exports = {
   env: {
     es6: true,
     jest: true,
+    // Whitelist all browser-like globals.
     "react-native/react-native": true
   },
   parser: "babel-eslint",
   parserOptions: {
+    // Enables ecmaFeatures modules
     sourceType: "module",
+    // Enables ecmaFeatures experimentalObjectRestSpread
     ecmaVersion: 2018,
     ecmaFeatures: {
       jsx: true
     }
   },
-  extends: ["airbnb", "plugin:prettier/recommended", "prettier/react"],
-  plugins: ["eslint-comments", "react-native", "jest"],
+  extends: [
+    // https://www.npmjs.com/package/eslint-config-airbnb
+    "airbnb",
+    // https://www.npmjs.com/package/eslint-plugin-prettier
+    "plugin:prettier/recommended",
+    // We override Airbnb with prettier, and add React prettier plugins
+    "prettier/react"
+  ],
+  plugins: [
+    // https://www.npmjs.com/package/eslint-plugin-eslint-comments
+    "eslint-comments",
+    // https://www.npmjs.com/package/eslint-plugin-react-native
+    "react-native",
+    // https://www.npmjs.com/package/eslint-plugin-jest
+    "jest"
+  ],
   rules: {
+    // Enables parseInt without second parameter of radix.
     radix: ["error", "as-needed"],
+    // Enables using object methods without Object.prototype.method.
     "no-prototype-builtins": "off",
+    // Enables ++.
     "no-plusplus": "off",
+    // Ignores unused vars named _.
     "no-unused-vars": [
       "error",
       {
         argsIgnorePattern: "^_"
       }
     ],
+    // Enables reassignment of function parameters.
     "no-param-reassign": "off",
-    "no-return-assign": "off",
-    "react/prop-types": "off",
-    "react/sort-comp": "off",
+
+    // React Plugin - eslint-plugin-react
+    // Enforces PropType validation only if declared PropTypes block.
+    "react/prop-types": ["error", { skipUndeclared: true }],
+    // Sorts Static Methods, Static Variables, Instance Variables, Lifecycle, Everything Else, Event Methods, Rendering Methods.
+    "react/sort-comp": [
+      "warn",
+      {
+        order: [
+          "static-methods",
+          "static-variables",
+          "instance-variables",
+          "lifecycle",
+          "everything-else",
+          "/^on.+$/",
+          "rendering"
+        ],
+        groups: {
+          rendering: ["/^render.+$/", "render"]
+        }
+      }
+    ],
+    // Enables PropTypes of any, object, array.
     "react/forbid-prop-types": "off",
+    // Enforces JSX only in .js and .jsx files.
     "react/jsx-filename-extension": [
       "error",
       {
         extensions: [".js", ".jsx"]
       }
     ],
+
+    // ESLint Comments Plugin - eslint-plugin-eslint-comments
+    // Disables eslint-enable comments for multiple eslint-disable comments.
+    "eslint-comments/no-aggregating-enable": "error",
+    // Disables eslint-disable comments without rule names.
+    "eslint-comments/no-unlimited-disable": "error", 
+    // Disables eslint-disable that don't cover any errors.
+    "eslint-comments/no-unused-disable": "error",
+    // Disables eslint-enable that don't enable anything or enable rules that weren't disabled.
+    "eslint-comments/no-unused-enable": "error",
+
+    // React-Native Plugin - eslint-plugin-react-native
+    // Enforces Static Properties as static public field.
+    "react/static-property-placement": ["error"],
+    // Enforces State as static field.
+    "react/state-in-constructor": ["error", "never"],
+    // Enables JSX Spreading.
+    "react/jsx-props-no-spreading": "off",
+    // Enforces no unused StyleSheets.
     "react-native/no-unused-styles": "error",
+    // Enforces no inline styles.
     "react-native/no-inline-styles": "error",
+    // Enforces no single-element style arrays.
     "react-native/no-single-element-style-arrays": "error",
-    "jest/no-disabled-tests": "warn",
-    "jest/no-focused-tests": "warn",
-    "jest/no-identical-title": "warn",
-    "jest/valid-expect": "warn"
+    // Disables commiting disabled tests.
+
+    // Jest Plugin - eslint-plugin-jest
+    "jest/no-disabled-tests": "error",
+    // Disables commiting focused tests (run only one test).
+    "jest/no-focused-tests": "error",
+    // Disables commiting duplicate named test-suites or tests.
+    "jest/no-identical-title": "error",
+    // Disables commiting empty expect assertions.
+    "jest/valid-expect": "error"
   }
 };
