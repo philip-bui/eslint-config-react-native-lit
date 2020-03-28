bb5c4aae8542862fdc87046cbf1e8502a2c3ad9e
Philip <philip.bui.developer@gmail.com>
2020-03-28 23:35:52 +1100

    Version 1.0.3 (#4)
    - Added eslint-plugin-react-redux.
    - Added react-redux recommended rules.
    - Updated eslint-comment rules to recommended rules.
    - Updated jest rules to recommended.

--- index.js
+++ index.js
@@ -15,26 +15,36 @@ module.exports = {
       jsx: true
     }
   },
-  extends: [
-    // https://www.npmjs.com/package/eslint-config-airbnb
-    "airbnb",
-    // https://www.npmjs.com/package/eslint-plugin-prettier
-    "plugin:prettier/recommended",
-    // We override Airbnb with prettier, and add React prettier plugins
-    "prettier/react"
-  ],
   plugins: [
     // https://www.npmjs.com/package/eslint-plugin-eslint-comments
     "eslint-comments",
     // https://www.npmjs.com/package/eslint-plugin-react-native
     "react-native",
     // https://www.npmjs.com/package/eslint-plugin-jest
-    "jest"
+    "jest",
+    // https://www.npmjs.com/package/eslint-plugin-react-redux
+    "react-redux"
+  ],
+  extends: [
+    // https://mysticatea.github.io/eslint-plugin-eslint-comments/rules/
+    "plugin:eslint-comments/recommended",
+    // https://www.npmjs.com/package/eslint-plugin-jest#recommended
+    "plugin:jest/recommended",
+    // https://www.npmjs.com/package/eslint-plugin-jest#style
+    "plugin:jest/style",
+    // https://www.npmjs.com/package/eslint-plugin-react-redux#supported-rules
+    "plugin:react-redux/recommended",
+    // https://www.npmjs.com/package/eslint-config-airbnb
+    "airbnb",
+    // https://www.npmjs.com/package/eslint-plugin-prettier#recommended-configuration
+    "plugin:prettier/recommended",
+    // We override Airbnb with prettier, and add React prettier plugins
+    "prettier/react"
   ],
   rules: {
     // Enables parseInt without second parameter of radix.
     radix: ["error", "as-needed"],
-    // Enables using object methods without Object.prototype.method.
+    // Enables using prototype methods without Object.prototype.method.
     "no-prototype-builtins": "off",
     // Enables ++.
     "no-plusplus": "off",
@@ -47,6 +57,12 @@ module.exports = {
     ],
     // Enables reassignment of function parameters.
     "no-param-reassign": "off",
+    // Enables returning of assignment for this.ref = ref.
+    "no-return-assign": "off",
+
+    // Import Plugin - eslint-plugin-import
+    // Recommends using default export for single-export files.
+    "import/prefer-default-export": "warn",
 
     // React Plugin - eslint-plugin-react
     // Enforces PropType validation only if declared PropTypes block.
@@ -78,24 +94,13 @@ module.exports = {
         extensions: [".js", ".jsx"]
       }
     ],
-
-    // ESLint Comments Plugin - eslint-plugin-eslint-comments
-    // Disables eslint-enable comments for multiple eslint-disable comments.
-    "eslint-comments/no-aggregating-enable": "error",
-    // Disables eslint-disable comments without rule names.
-    "eslint-comments/no-unlimited-disable": "error",
-    // Disables eslint-disable that don't cover any errors.
-    "eslint-comments/no-unused-disable": "error",
-    // Disables eslint-enable that don't enable anything or enable rules that weren't disabled.
-    "eslint-comments/no-unused-enable": "error",
-
-    // React-Native Plugin - eslint-plugin-react-native
     // Enforces Static Properties as static public field.
     "react/static-property-placement": ["error", "static public field"],
     // Enforces State as static field.
     "react/state-in-constructor": ["error", "never"],
     // Enables JSX Spreading.
     "react/jsx-props-no-spreading": "off",
+    // React-Native Plugin - eslint-plugin-react-native
     // Enforces no unused StyleSheets.
     "react-native/no-unused-styles": "error",
     // Enforces no inline styles.
@@ -104,13 +109,15 @@ module.exports = {
     "react-native/no-single-element-style-arrays": "error",
 
     // Jest Plugin - eslint-plugin-jest
-    // Disables commiting disabled tests.
-    "jest/no-disabled-tests": "error",
-    // Disables commiting focused tests (run only one test).
-    "jest/no-focused-tests": "error",
-    // Disables commiting duplicate named test-suites or tests.
-    "jest/no-identical-title": "error",
-    // Disables commiting empty expect assertions.
-    "jest/valid-expect": "error"
+    // Enforces a top-level describe block.
+    "jest/require-top-level-describe": "warn",
+    // Enforces valid describe() callbacks.
+    "jest/valid-describe": "error",
+    // Enforces valid titles for jest blocks.
+    "jest/valid-title": "error",
+
+    // React-Redux Plugin - eslint-plugin-react-redux
+    // Enables connected component and component in single file.
+    "react-redux/prefer-separate-component-file": "off"
   }
 };
